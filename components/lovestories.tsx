d28dc7569449f7e34e18c2a8344a839b3c758842
TeeJayGTAN <oniretemisan@gmail.com>
2025-05-13 17:07:19 +0100

    extra video fixes

--- components/lovestories.tsx
+++ components/lovestories.tsx
@@ -28,8 +28,8 @@ export default function LoveStories() {
 function Item({ i }: { i: any }) {
   return (
     <div className="bg-white w-full h-full p-[40px] flex flex-col gap-[20px] items-center justify-center shadow-md rounded-[10px]">
-      <div className="w-full relative ">
-        <div className=" w-full h-full relative">
+      <div className="w-full relative flex justify-center items-center">
+        <div className=" w-full h-full relative rounded overflow-hidden">
            <video autoPlay  loop id="myVideo">
             <source src={i.video} type="video/mp4" />
           </video>
